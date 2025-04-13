e0d88df445e6c4da18358a442f590689235e431c
BotaNoel <77171449+BotaNoel@users.noreply.github.com>
2025-04-13 19:15:09 +0200

    kep

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -9,8 +9,22 @@ class PhotoController extends Controller
 {
     public function store(Request $request)
     {
-        $photo = Photo::create($request->all());
+        $request->validate([
+            'apartment_id' => 'required|exists:apartments,id',
+            'photo' => 'required|image|mimes:jpeg,png,jpg|max:2048'
+        ]);
 
-        return response()->json($photo, 201);
+        if ($request->hasFile('photo')) {
+            $path = $request->file('photo')->store('photos', 'public');
+
+            $photo = Photo::create([
+                'apartment_id' => $request->apartment_id,
+                'url' => asset('storage/' . $path),
+            ]);
+
+            return response()->json($photo, 201);
+        }
+
+        return response()->json(['error' => 'Fájl nem érkezett'], 400);
     }
 }
